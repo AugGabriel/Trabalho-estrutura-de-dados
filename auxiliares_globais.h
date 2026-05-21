bbf21e47f611f2d525d8429d1187079bc5847b6f
GabrielAugusto <gabriel.galup@gmail.com>
2026-05-21 08:34:44 -0300

    Implementação de mais dois arquivos .c

--- auxiliares_globais.h
+++ auxiliares_globais.h
@@ -13,10 +13,6 @@
 
 // Inicializa string com caracteres de final, para garantir que ela termine em algum momento
 // O ideal seria que a entrada com o %s encerrasse a string com \0, porém o comportamento pareceu variar entre compiladores
-void inicializa_string(char *string, const int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        string[i] = '\0';
-    }
-}
+void inicializa_string(char *string, const int tamanho);
 
 #endif
